a92db466b4f59bfb10d04853b193f308fa0457b9
naekit <mykyta.onishchuck@code.berlin>
2022-12-15 09:41:20 -0500

    front end

---
+++ graphMgmtApp/client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <nav className="navbar bg-light mb-4 p-0">
+        <div className="container">
+            <a href="/" className="navbar-brand">
+                <div className="d-flex">
+                    <div>Client System</div>
+                </div>
+            </a>
+        </div>
+    </nav>
+  )
+}
